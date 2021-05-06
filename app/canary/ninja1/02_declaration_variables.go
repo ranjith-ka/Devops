adb12450415b23f5d73430bfd18b9878ae9a43c7
jaswanth <singamchettyjaswanth143@gmail.com>
2021-05-06 07:01:04 +0530

    Create 02_declaration_variables.go

---
+++ app/canary/ninja1/02_declaration_variables.go
@@ -0,0 +1,17 @@
+// Declare variable and print
+package main
+
+import (
+	"fmt"
+)
+
+var x int = 123
+var y string = "jsm" //variable group declaration
+var z bool = true
+
+func main() {
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
