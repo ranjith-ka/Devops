e0351d0a9ea97805712cc24a3470d3c5390f4790
jaswanth <singamchettyjaswanth143@gmail.com>
2021-05-06 07:08:44 +0530

    Update 02_declaration_variables.go

--- app/canary/ninja1/02_declaration_variables.go
+++ app/canary/ninja1/02_declaration_variables.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 )
 
-var x int = 123
-var y string = "jsm" //variable group declaration
-var z bool = true
+//var x int = 123
+//var y string = "jsm" //variable group declaration
+//var z bool = true
+
+var (
+	x int    = 123
+	y string = "jsm" //variable group declaration
+	z bool   = true
+)
 
 func main() {
 	fmt.Println(x, y, z)
