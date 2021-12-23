aeca3b1cac864ce99ee6e6f21eaa04835ca927b8
Ranjith KA <ranjithkumar.359@gmail.com>
2021-12-24 00:07:54 +0530

    Fixing vscode go extension issues

--- app/canary/ninja9_waitgroup/01_waitgroup_print.go
+++ app/canary/ninja9_waitgroup/01_waitgroup_print.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
-func main(){
-    var wg sync.WaitGroup
-    wg.Add(2)
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-    go func(){
-        fmt.Println("Realme offers here")
-        wg.Done()
-    }()
+	go func() {
+		fmt.Println("Realme offers here")
+		wg.Done()
+	}()
 
-    go func(){
-        fmt.Println("Oppo offers here")
-        wg.Done()
-    }()
+	go func() {
+		fmt.Println("Oppo offers here")
+		wg.Done()
+	}()
 
-    // Uncomment the Wait to check the new offers, else you might miss the offers
-    wg.Wait()
-    fmt.Println("Check the new mobiles with offers, make sure you didn't miss in concurrency")
+	// Uncomment the Wait to check the new offers, else you might miss the offers
+	wg.Wait()
+	fmt.Println("Check the new mobiles with offers, make sure you didn't miss in concurrency")
 }
--- app/golangbootcamp/methods.go
+++ app/golangbootcamp/methods.go
@@ -3,28 +3,27 @@ package main
 import "fmt"
 
 type User struct {
-    Id             int
-    Name, Location string
+	Id             int
+	Name, Location string
 }
 
 func (u *User) Greetings() string {
-    return fmt.Sprintf("Hi %s from %s",
-        u.Name, u.Location)
+	return fmt.Sprintf("Hi %s from %s",
+		u.Name, u.Location)
 }
 
 type Player struct {
-    *User
-    GameId int
+	*User
+	GameId int
 }
-
 
 func NewPlayer(id int, name, location string, gameId int) *Player {
-    return &Player{
-        User: &User{id, name, location},
-        GameId: gameId,
-    }
+	return &Player{
+		User:   &User{id, name, location},
+		GameId: gameId,
+	}
 }
 func main() {
-    p := NewPlayer(42, "Matt", "LA", 90404)
-    fmt.Println(p.Greetings())
+	p := NewPlayer(42, "Matt", "LA", 90404)
+	fmt.Println(p.Greetings())
 }
