aaea491c731be22ec3d226519ac1b8ad91e3ecee
Ranjith KA <ranjithkumar.359@gmail.com>
2022-01-04 20:24:50 +0530

    Add few more changes for my learning

--- app/canary/ninja2_operators/03_operators_const.go
+++ app/canary/ninja2_operators/03_operators_const.go
@@ -30,11 +30,11 @@ func main() {
 
 	// just try some random examples for slices, https://go.dev/blog/slices
 	slice2 := sliceHeader{
-		Length:        50,
-		ZerothElement: &buffer[100],
+		Length:        5,
+		ZerothElement: &buffer[0],
 	}
 
-	slice := buffer[100:150]
+	slice := buffer[100:120]
 
 	// Just Print the slice for reference
 	fmt.Println(slice)
@@ -50,4 +50,22 @@ func main() {
 	fmt.Println(slice2)
 	slashPos := bytes.IndexRune(slice, '/')
 	fmt.Println(slashPos)
+
+	slice = buffer[100:120]
+
+	fmt.Println(slice)
+	for i := 0; i < len(slice); i++ {
+		slice[i] = byte(i)
+	}
+	fmt.Println("before", slice)
+	AddOneToEachElement(slice)
+	fmt.Println("after", slice)
+
+}
+
+// passing slice to function
+func AddOneToEachElement(slice []byte) {
+	for i := range slice {
+		slice[i]++
+	}
 }
