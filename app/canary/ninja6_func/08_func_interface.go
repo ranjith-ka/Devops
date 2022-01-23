31bd16fa6b573b8373a854076140d9d151a96069
Ranjith KA <ranjithkumar.359@gmail.com>
2022-01-24 01:06:48 +0530

    Keepcoding interface practise

--- app/canary/ninja6_func/08_func_interface.go
+++ app/canary/ninja6_func/08_func_interface.go
@@ -1,8 +1,52 @@
+// Keepcoding Interface
 package main
 
-type animal struct{
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+)
 
+const format = "%v: Info: %s\n"
+
+// Logger interface to implement the methods
+type Logger interface {
+	Info(string)
 }
-func main(){
+
+type ScreenLogger struct {
+}
+
+func (s ScreenLogger) Info(message string) {
+	fmt.Printf(format, time.Now(), message)
+}
+
+type FileLogger struct {
+	File os.File
+}
+
+func (l *FileLogger) Info(message string) {
+	fmt.Fprint(&l.File, format, time.Now(), message)
+}
+
+func NewFileLogger(filename string) *FileLogger {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		panic(err)
+	}
+	return &FileLogger{*file}
+}
+
+func main() {
+	var log Logger        // Interface type
+	log = &ScreenLogger{} // still works with interface method object type
+	log.Info("hello guys")
+
+	// Reassign for same variable
+	log = NewFileLogger("log.txt")
+	log.Info("hello guys")
+
+	fmt.Println(reflect.TypeOf(log)) //Print the type of log variable
 
 }
