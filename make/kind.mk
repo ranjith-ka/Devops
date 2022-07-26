cfe8d95beed01a70f78773c9b45b6b22be476f2a
Ranjith KA <ranjithkumar.359@gmail.com>
2022-07-26 15:56:39 +0530

    Update Kind to 1.22

--- make/kind.mk
+++ make/kind.mk
@@ -6,11 +6,11 @@ kind: kind-cluster
 
 kind-cluster:
 	@echo Creating Kind environment
-	@kind create cluster --config kind/config.yaml --name k8s-1.21.1
+	@kind create cluster --config kind/config.yaml --name k8s-1.22.9
 
 load-image:
-	@kind load docker-image  ranjithka/canary:0.0.1  --name k8s-1.21.1
-	@kind load docker-image  ranjithka/canary:latest --name k8s-1.21.1
+	@kind load docker-image  ranjithka/canary:0.0.1  --name k8s-1.22.9
+	@kind load docker-image  ranjithka/canary:latest --name k8s-1.22.9
 
 delete-kind:
-	@kind delete cluster --name k8s-1.21.1
+	@kind delete cluster --name k8s-1.22.9
